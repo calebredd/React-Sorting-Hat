b0fc8ac7ee3613eaf4dd5de844e3e895686bd575
Caleb Redd <caleb.redd@yahoo.com>
2019-09-03 19:51:18 -0700

    Styling, footer, and img changes

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.scss";
+import Footer from "./components/Footer.js";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Hogwarts!</h1>
+          <img
+            className="logo"
+            src="https://i.pinimg.com/originals/e5/41/d2/e541d2f8d9bf187dd57a76ab74899051.gif"
+            alt="Hogwarts Emblem"
+          />
+          <h2>You're a wizard, Harry</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="hat">
+          <button className="hatButton">Find your House!</button>
+          <br />
+          <img
+            className="hatImg"
+            src="https://images.immediate.co.uk/production/volatile/sites/3/2016/06/53243.jpg?quality=90&resize=620,413"
+            alt="Sorting Hat"
+          />
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ react-sorting-hat/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../footer.scss";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>Gryffindor</span>
+      <span>Hufflepuff</span>
+      <span>Ravenclaw </span>
+      <span>Slytherin</span>
+    </div>
+  );
+}
